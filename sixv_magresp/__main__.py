c11abdd9b9544d163b9b4392d52711b291836741
Anton <Umarov@gtlab.local>
2022-03-21 17:05:56 +0300

    21.03.22

--- sixv_magresp/__main__.py
+++ sixv_magresp/__main__.py
@@ -1,5 +1,16 @@
+from json import load as load_json
+from rich import print as print_rich
+import importlib.resources
+
+
+def get_config():
+    with importlib.resources.open_text(__package__, "config.json") as file:
+        return load_json(file)
+
+
 def main():
-    pass
+    config = get_config()
+    print_rich(config)
 
 
 if __name__ == "__main__":
