704c69f289863b75ad9caa4833150c2275dff3c7
Anton <Umarov@gtlab.local>
2022-03-22 16:23:53 +0300

    22.03.22

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+requirements.append(
+    "gtrfile @ git+https://github.com/spectral307/gtrfile.git#egg=gtrfile-1.0.0")
+
 setup(name="sixv_magresp",
       version="0.0.0",
       packages=["sixv_magresp"],
